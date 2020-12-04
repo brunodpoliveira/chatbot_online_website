4c4c282b4e1e41b2fd8265693c9403964b013d38
Bruno Daniel <70975013+brunodpoliveira@users.noreply.github.com>
2020-12-04 09:20:50 -0300

    Update send_mail.py

--- test-algos/send_mail.py
+++ test-algos/send_mail.py
@@ -6,14 +6,14 @@ from email.mime.text import MIMEText
 
 def send_mail(customer, emotion, comments):
     port = 2525
-    smtp_server = 'smtp.mailtrap.io'
+    smtp_server = ''
     login = ''
     password = ''
     message = f"<h3>New feedback Submission</h3><ul><li>Cliente: {customer}</li></ul>" \
               f"<ul><li>emoção: {emotion}</li></ul>" \
               f"<ul><li>frase: {comments}</li></ul>"
-    sender_email = 'email1@example.com'
-    receiver_email = 'email2@example.com'
+    sender_email = ''
+    receiver_email = ''
     msg = MIMEText(message, 'html')
     msg['Subject'] = 'Feedback Chatbot'
     msg['From'] = sender_email
